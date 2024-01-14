d3d1a3d15ec06afac4052e991cb0fa3213906808
nicelogic <358844436@qq.com>
2024-01-14 14:34:47 +0800

    v1.2.4

---
+++ src/go.mod
@@ -0,0 +1,7 @@
+module github.com/nicelogic/tokendecoder
+
+go 1.21.0
+
+retract v2.0.0+incompatible
+
+require github.com/golang-jwt/jwt/v5 v5.2.0
---
+++ src/variable/variable.go
@@ -0,0 +1,9 @@
+package variable
+
+import "github.com/nicelogic/tokendecoder/model"
+
+var (
+	JwtMapCliamsKeyUserInfo = "user_info"
+	UserCtxKey  = &model.ContextKey{Name: "user"}
+	ErrorCtxKey = &model.ContextKey{Name: "error"}
+)
