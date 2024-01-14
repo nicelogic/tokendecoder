6057ab039e8dea31727db56fa20169741a0feda5
nicelogic <358844436@qq.com>
2024-01-14 15:47:52 +0800

    v1.3.1

--- src/tokendecoder.go
+++ src/tokendecoder.go
@@ -22,7 +22,7 @@ type TokenDecoder struct {
 func (decoder *TokenDecoder) Init(publicKeyFilePath string) error {
 	publicKey, err := os.ReadFile(publicKeyFilePath)
 	if err != nil {
-		return fmt.Errorf("read public key file(%v).error(%v)", publicKeyFilePath, err)
+		return fmt.Errorf("os.read public key file(%v).error(%v)", publicKeyFilePath, err)
 	}
 	decoder.publicKey, err = jwt.ParseRSAPublicKeyFromPEM(publicKey)
 	if err != nil {
